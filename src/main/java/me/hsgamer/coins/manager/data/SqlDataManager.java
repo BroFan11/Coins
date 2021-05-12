1aebb899895887ceabc4255b110a5422e3a7ef56
hsgamer <huynhqtienvtag@gmail.com>
2021-05-12 20:25:42 +0700

    fix mysql

--- src/main/java/me/hsgamer/coins/manager/data/SqlDataManager.java
+++ src/main/java/me/hsgamer/coins/manager/data/SqlDataManager.java
@@ -4,51 +4,103 @@ import me.hsgamer.hscore.database.client.sql.PreparedStatementContainer;
 import me.hsgamer.hscore.database.client.sql.SqlClient;
 import org.intellij.lang.annotations.Language;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.UUID;
 
 public abstract class SqlDataManager implements DataManager {
-    private SqlClient<?> client;
+    private Connection connection;
 
     protected abstract SqlClient<?> createClient();
 
     @Override
     public void setup() {
-        this.client = createClient();
+        SqlClient<?> client = createClient();
         client.prepareStatementSafe(Query.CREATE.queryCommand).ifPresent(PreparedStatementContainer::updateSafe);
+        try {
+            connection = client.getConnection();
+        } catch (SQLException exception) {
+            throw new IllegalStateException("Cannot get connection", exception);
+        }
     }
 
     @Override
     public void disable() {
-        // EMPTY
+        try {
+            connection.close();
+        } catch (SQLException exception) {
+            throw new IllegalStateException("Cannot close connection", exception);
+        }
+    }
+
+    private PreparedStatementContainer prepareStatement(String query, Object... args) {
+        PreparedStatementContainer container;
+        try {
+            container = PreparedStatementContainer.of(connection, query, args);
+        } catch (SQLException exception) {
+            throw new IllegalStateException("Cannot prepare statement", exception);
+        }
+        return container;
+    }
+
+    private boolean hasProfile(UUID uuid) {
+        try (PreparedStatementContainer container = prepareStatement(Query.GET.queryCommand, uuid)) {
+            return container.querySafe()
+                    .map(resultSet -> {
+                        try {
+                            return resultSet.next();
+                        } catch (SQLException exception) {
+                            exception.printStackTrace();
+                            return false;
+                        }
+                    }).orElse(false);
+        } catch (SQLException exception) {
+            throw new IllegalStateException("Cannot close statement", exception);
+        }
     }
 
     @Override
     public long getCoin(UUID uuid) {
-        return client.prepareStatementSafe(Query.GET.queryCommand, uuid.toString()).flatMap(PreparedStatementContainer::querySafe)
-                .map(resultSet -> {
-                    try {
-                        if (resultSet.next()) {
-                            return resultSet.getLong("coin");
-                        } else {
-                            return 0L;
+        try (PreparedStatementContainer container = prepareStatement(Query.GET.queryCommand, uuid)) {
+            return container.querySafe()
+                    .map(resultSet -> {
+                        try {
+                            if (resultSet.next()) {
+                                return resultSet.getLong("coin");
+                            } else {
+                                return 0L;
+                            }
+                        } catch (SQLException exception) {
+                            exception.printStackTrace();
                         }
-                    } catch (SQLException exception) {
-                        exception.printStackTrace();
-                    }
-                    return -1L;
-                }).orElse(0L);
+                        return -1L;
+                    }).orElse(0L);
+        } catch (SQLException exception) {
+            throw new IllegalStateException("Cannot close statement", exception);
+        }
     }
 
     @Override
     public void setCoin(UUID uuid, long coin) {
-        client.prepareStatementSafe(Query.SET.queryCommand, uuid.toString(), coin, coin).ifPresent(PreparedStatementContainer::updateSafe);
+        PreparedStatementContainer container;
+        if (hasProfile(uuid)) {
+            container = prepareStatement(Query.SET.queryCommand, coin, uuid.toString());
+        } else {
+            container = prepareStatement(Query.INSERT.queryCommand, uuid.toString(), coin);
+        }
+        container.updateSafe();
+        try {
+            container.close();
+        } catch (SQLException exception) {
+            throw new IllegalStateException("Cannot close statement", exception);
+        }
     }
 
     public enum Query {
         CREATE("CREATE TABLE IF NOT EXISTS coins (uuid VARCHAR(100), coin INT(32), PRIMARY KEY (uuid))"),
+        INSERT("INSERT INTO coins(uuid, coin) VALUES(?, ?)"),
         GET("SELECT * FROM coins WHERE uuid = ?"),
-        SET("INSERT INTO coins (uuid, coin) VALUES (?,?) ON DUPLICATE KEY UPDATE coin = ?");
+        SET("UPDATE coins SET coin = ? WHERE uuid = ?");
 
         @Language("SQL")
         private final String queryCommand;
