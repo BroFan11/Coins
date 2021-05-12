c6af2a7511144e702a1f5b7ecd02aae0d05b71f5
hsgamer <huynhqtienvtag@gmail.com>
2021-05-12 19:59:29 +0700

    set and take

---
+++ src/main/java/me/hsgamer/coins/command/SetCoinCommand.java
@@ -0,0 +1,58 @@
+package me.hsgamer.coins.command;
+
+import me.hsgamer.coins.Permissions;
+import me.hsgamer.coins.api.CoinsAPI;
+import me.hsgamer.coins.config.MessageConfig;
+import me.hsgamer.hscore.bukkit.utils.MessageUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SetCoinCommand extends Command {
+    public SetCoinCommand() {
+        super("setcoin", "Set coins to a player", "/setcoin <player> <coin>", Collections.emptyList());
+        setPermission(Permissions.SET_COIN.getName());
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
+        if (!testPermission(sender)) {
+            return false;
+        }
+        if (args.length < 2) {
+            MessageUtils.sendMessage(sender, getUsage());
+            return false;
+        }
+        long coin;
+        try {
+            coin = Long.parseLong(args[1]);
+        } catch (Exception e) {
+            MessageUtils.sendMessage(sender, MessageConfig.NUMBER_ONLY.getValue());
+            return false;
+        }
+        // noinspection deprecation
+        UUID uuid = Bukkit.getOfflinePlayer(args[0]).getUniqueId();
+        CoinsAPI.setCoin(uuid, coin);
+        MessageUtils.sendMessage(sender, MessageConfig.SUCCESS.getValue());
+        return false;
+    }
+
+    @Override
+    public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException {
+        if (args.length == 1) {
+            return super.tabComplete(sender, alias, args);
+        } else if (args.length == 2) {
+            return IntStream.range(1, 100).boxed().map(i -> Integer.toString(i)).collect(Collectors.toList());
+        } else {
+            return Collections.emptyList();
+        }
+    }
+}
